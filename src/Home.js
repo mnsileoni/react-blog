1bbf93bb1b788d06f095b50442274c09710b30b1
Marcelino Sileoni <mnsileoni@gmail.com>
2022-11-16 17:17:14 -0300

    Chapter21a

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,14 @@
 import Feed from './Feed';
+import { useContext } from 'react';
+import DataContext from './context/DataContext';
 
-const Home = ({ posts, fetchError, isLoading }) => {
+const Home = () => {
+    const {searchResults, fetchError, isLoading} = useContext(DataContext);
     return (
         <main className="Home">
             {isLoading && <p className="statusMsg">Cargando posts...</p>}
             {!isLoading && fetchError && <p className="statusMsg" style={{ color: "red" }}>{fetchError}</p>}
-            {!isLoading && !fetchError && (posts.length ? <Feed posts={posts} /> : <p className="statusMsg">No hay posts a mostrar.</p>)}
+            {!isLoading && !fetchError && (searchResults.length ? <Feed posts={searchResults} /> : <p className="statusMsg">No hay posts a mostrar.</p>)}
         </main>
     )
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-
-
-//const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
   <React.StrictMode>
     <Router>
